c299caa8aca9f76b42eb75abcee083c4585fece0
evan bernstein <evan@foo.net>
2025-11-23 22:15:17 -0500

    add Triscuits filter

--- apps/web/app/components/song/song-filters.ts
+++ apps/web/app/components/song/song-filters.ts
@@ -2,6 +2,7 @@ export const SONG_FILTERS = {
   "Sammy Era": { endDate: new Date("2005-08-27") },
   "Allen Era": { startDate: new Date("2005-12-28"), endDate: new Date("2025-09-07") },
   "Marlon Era": { startDate: new Date("2025-10-31") },
+  Triscuits: { startDate: new Date("2000-03-11"), endDate: new Date("2000-07-12") },
 };
 
 export const NOT_PLAYED_SONG_FILTER_PARAM = "Not Played";
