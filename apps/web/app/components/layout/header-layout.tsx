d320cb3f3e67d3ce4c6a4bce47a410fa7db654d9
Don Cote <mistercote@gmail.com>
2025-12-29 18:26:12 -0500

    nudge padding

--- apps/web/app/components/layout/header-layout.tsx
+++ apps/web/app/components/layout/header-layout.tsx
@@ -6,7 +6,7 @@ export function HeaderLayout({ children }: { children: React.ReactNode }) {
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="pt-16 flex-1">
-        <div className="px-2 py-1 sm:px-6 lg:px-8 sm:py-4">{children}</div>
+        <div className="px-3 py-1 sm:px-6 lg:px-8 sm:py-4">{children}</div>
       </main>
       <Footer />
     </div>
